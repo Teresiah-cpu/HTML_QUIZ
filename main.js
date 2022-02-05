cfacb54c233139f8593594717dd81b6cd9c6da23
Teresiah-cpu <terrywambo2018@gmail.com>
2022-02-05 18:18:00 +0300

    restructured the button and added a function in main.js file

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 function check() {
     var c=0;
     var q1=document.quiz.question1.value;
-    if (q1=="Object Oriented Programming Language") {c++}
+    var q2=document.quiz.question2.value;
+    var q3=document.quiz.question3.value;
+    var q4=document.quiz.question4.value;
+    var q5=document.quiz.question5.value;
+    var q6=document.quiz.question6.value;
+    if (q1=="A markup language") {c++}
+    if (q2=="Scope") {c++}
+    if (q3=="Global variable") {c++}
+    if (q4=="Local") {c++}
+    if (q5=="var,const,let") {c++}
+    if (q6=="React") {c++}
         document.write(c);
 }
